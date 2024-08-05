9841b1fd5e963f51b1ec63520d4964623afc57d7
Salman Farshi <devwithfarshi@gmail.com>
2024-08-05 17:53:43 +0600

    ✅: add: Banner and perks section added

---
+++ src/components/screen/Home/Banner.tsx
@@ -0,0 +1,33 @@
+import MaxWidthWrapper from "@/components/common/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import routenames from "@/data/rotues.data";
+import { MoveRight } from "lucide-react";
+import Link from "next/link";
+
+const Banner = () => {
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Your marketplace for high-quality{" "}
+          <span className="text-blue-600">Digital assets</span>.
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Welcome to Secure Market. Evret assets on our platform is verified by
+          our team to ensure our highest quality standards.
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href={routenames.products} className={buttonVariants()}>
+            Browse Trending
+          </Link>
+          <Button variant={"ghost"}>
+            Our quality promise
+            <MoveRight className="text-gray-900 ml-2 size-4" />
+          </Button>
+        </div>
+      </div>
+      {/* TODO: List pordcuts */}
+    </MaxWidthWrapper>
+  );
+};
+export default Banner;
---
+++ src/data/rotues.data.ts
@@ -0,0 +1,5 @@
+const routenames = {
+  home: "/",
+  products: "/prodcuts",
+};
+export default routenames;
