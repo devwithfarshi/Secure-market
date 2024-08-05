6595ac712402e738d0f5b92bda5be145320cb7a5
Salman Farshi <devwithfarshi@gmail.com>
2024-08-06 00:41:21 +0600

    ✅: add: navitem added with close

--- src/components/sheard/Navbar/NavItems.tsx
+++ src/components/sheard/Navbar/NavItems.tsx
@@ -1,4 +1,48 @@
+"use client";
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useEffect, useRef, useState } from "react";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/useOutsideClick";
+
 const NavItems = () => {
-  return <div>NavItems</div>;
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
+  const navRef = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", handler);
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+  return (
+    <div ref={navRef} className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, index) => {
+        const handleOpen = () => {
+          if (activeIndex === index) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(index);
+          }
+        };
+        const isOpen = index === activeIndex;
+        return (
+          <NavItem
+            key={index}
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 export default NavItems;
